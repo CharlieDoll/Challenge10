2cbb289ebfc400ea55eb2fa353e7b6c4cf12ec53
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-03-08 16:41:25 +0000

    index.js questions and menu added

--- index.js
+++ index.js
@@ -82,10 +82,97 @@ function mainMenu() {
         // Something else went wrong
       }
     });
-}
 
-function createEngineer() {
-  // ask qs about engineer - use manager ex.
-}
+  function createEngineer() {
+    // name, ID, email, Github username
+    // ask qs about engineer - use manager ex.
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the Engineer's Name?",
+        },
+        {
+          type: "input",
+          name: "engineerID",
+          message: "What is the engineer's ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is their email address?",
+        },
+        {
+          type: "input",
+          name: "githubName",
+          message: "What is their Github username?",
+        },
+      ])
 
-function createIntern() {}
+      .then((answers) => {
+        //   console.log(answers);
+        const newEngineer = new Engineer(
+          answers.engineerName,
+          answers.engineerID,
+          answers.email,
+          answers.githubName
+        );
+        teamArray.push(newEngineer);
+        console.log(teamArray);
+        mainMenu();
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+  }
+
+  function createIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the Intern's Name?",
+        },
+        {
+          type: "input",
+          name: "internID",
+          message: "What is the intern's ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is their email address?",
+        },
+        {
+          type: "input",
+          name: "ointernSchool",
+          message: "Which school did they attend?",
+        },
+      ])
+      .then((answers) => {
+        //   console.log(answers);
+        const newIntern = new Intern(
+          answers.internName,
+          answers.internID,
+          answers.email,
+          answers.internSchool
+        );
+        teamArray.push(newIntern);
+        console.log(teamArray);
+        mainMenu();
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+  }
+}
